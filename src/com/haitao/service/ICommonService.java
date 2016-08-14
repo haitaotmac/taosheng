bcffeaab376cbd228036c7f7aff6eefae580bf9d
haitao <haitaojodan@126.com>
2016-08-14 11:00:33 +0800

    add init

---
+++ src/com/haitao/service/ICommonService.java
@@ -0,0 +1,5 @@
+package com.haitao.service;
+
+public interface ICommonService {
+	public abstract void add();
+}
---
+++ src/com/haitao/service/impl/CommonServiceImpl.java
@@ -0,0 +1,12 @@
+package com.haitao.service.impl;
+
+import com.haitao.service.ICommonService;
+
+public class CommonServiceImpl implements ICommonService {
+
+	@Override
+	public void add() {
+		System.out.println("test ............");
+	}
+
+}
